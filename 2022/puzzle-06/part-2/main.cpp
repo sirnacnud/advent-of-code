eb9b605d6584eedf09928d196c1ffedf3b460a5d
Duncan Cunningham <duncanc4@gmail.com>
2022-12-15 18:14:56 +0100

    Add puzzle 6

---
+++ 2022/puzzle-06/part-2/main.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+#include <fstream>
+#include <set>
+#include <string>
+
+int findEndOfMarker(const std::string& line) {
+    int start = 0;
+    int end = start + 13;
+    bool found = false;
+
+    std::set<char> characters;
+
+    while (!found) {
+        for (int i = start; i <= end; ++i) {
+            characters.insert(line[i]);      
+        }
+
+        if (characters.size() == 14) {
+            found = true;        
+        } else {
+            characters.clear();
+            start += 1;
+            end += 1;
+        }
+    }
+
+    return end;    
+}
+
+int main(int argc, char* argv[]) {
+    if (argc < 2) {
+        std::cerr << "Supply input file as first argument" << std::endl;
+        return -1;    
+    }
+
+    std::ifstream inputFile;
+    inputFile.open(argv[1]);
+
+    if (inputFile.is_open()) {
+        std::string inputLine;
+
+        while (getline(inputFile, inputLine)) {
+            int count = findEndOfMarker(inputLine);
+            std::cout << count + 1 << std::endl;
+        }
+    }
+}
