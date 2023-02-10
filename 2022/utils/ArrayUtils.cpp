47d904c38f71f710eee33924c402c057986c91ce
Duncan Cunningham <duncanc4@gmail.com>
2023-02-10 22:24:54 +0100

    Add missing namespace to print2dIntArray

--- 2022/utils/ArrayUtils.cpp
+++ 2022/utils/ArrayUtils.cpp
@@ -18,7 +18,7 @@ int** ArrayUtils::allocate2dIntArray(int rows, int columns) {
     return array;
 }
 
-void print2dIntArray(int** array, int rows, int columns) {
+void ArrayUtils::print2dIntArray(int** array, int rows, int columns) {
     for (size_t i = 0; i < rows; ++i) {
         for (size_t j = 0; j < columns; ++j) {
             std::cout << array[i][j] << ' ';
